8c2b20166b40eba5ecbfa79e3bf03682ce75dcb1
Ari558 <69749025+Ari558@users.noreply.github.com>
2021-03-01 20:47:42 -0600

    Carperta Dao

---
+++ LaboratorioProgramacionUno/“LaboratorioProgramacionUno”/Dao/listaDeUsuarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _LaboratorioProgramacionUno_.DAO
+{
+    class listaDeUsuarios
+    {
+        public string[] nombreUsu = new string[2] { "Carlos", "Eva" };
+        public string[] passwordUsu = new string[2] { "123", "123" };
+    }
+}
